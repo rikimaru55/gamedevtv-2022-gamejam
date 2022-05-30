628cbe6069e7f9167faece328f91637e52e38dad
Quilo <rikimaru55@gmail.com>
2022-05-30 14:42:09 -0700

    Added Quit button to main game pause.

--- Assets/Scripts/PauseControl.cs
+++ Assets/Scripts/PauseControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 /// <summary>
 /// Ripped straight from: 
 /// https://gamedevbeginner.com/the-right-way-to-pause-the-game-in-unity/#prevent_input_when_paused
@@ -10,6 +11,7 @@ public class PauseControl : MonoBehaviour
 {
     public SpriteRenderer pauseBackground;
     public static bool gameIsPaused;
+    public GameObject quitButton;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -25,12 +27,19 @@ public class PauseControl : MonoBehaviour
             Time.timeScale = 0f;
             pauseBackground.enabled = true;
             SoundManager.GetInstance().PauseMainMusicPlayer();
+            quitButton.SetActive(true);
         }
         else
         {
             pauseBackground.enabled = false;
             Time.timeScale = 1;
             SoundManager.GetInstance().PlayMainMusicPlayer();
+            quitButton.SetActive(false);
         }
+    }
+
+    public void OnQuitButtonClick()
+    {
+        SceneManager.LoadScene(Constants.MAIN_MENU_SCENE_Name);
     }
 }
